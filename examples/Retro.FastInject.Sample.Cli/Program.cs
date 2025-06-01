dc99ae1de916cc4f1659915973befb4fae76bd50
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-06-01 14:46:41 -0400

    Created a basic CLI example

---
+++ examples/Retro.FastInject.Sample.Cli/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleAppFramework;
+using Retro.FastInject.Sample.Cli;
+using Retro.FastInject.Sample.Cli.Services;
+
+var serviceProvider = new CliServiceProvider();
+var app = ConsoleApp.Create();
+app.Add<RootCliCommand>();
+
+await app.RunAsync(args);
---
+++ examples/Retro.FastInject.Sample.Cli/RootCliCommand.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleAppFramework;
+using Retro.FastInject.Sample.Cli.Services;
+using Retro.ReadOnlyParams.Annotations;
+
+namespace Retro.FastInject.Sample.Cli;
+
+public class RootCliCommand([ReadOnly] SingletonClass singletonDisposable,
+                            [ReadOnly] ScopedClass scopedDisposable,
+                            [ReadOnly] TransientClass transientDisposable) {
+
+  [Command("")]
+  public void RootCommand([Argument]string argument1, string option1) {
+    Console.WriteLine($@"Handler for '{GetType().FullName}' is run:");
+    Console.WriteLine($@"Value for {nameof(option1)} parameter is '{option1}'");
+    Console.WriteLine($@"Value for {nameof(argument1)} parameter is '{argument1}'");
+    Console.WriteLine();
+
+    Console.WriteLine($"Instance for {transientDisposable.Name} is available");
+    Console.WriteLine($"Instance for {scopedDisposable.Name} is available");
+    Console.WriteLine($"Instance for {singletonDisposable.Name} is available");
+    Console.WriteLine();
+  }
+
+  public void SubCommand() {
+    Console.WriteLine($@"Handler for '{GetType().FullName}' is run:");
+    Console.WriteLine($"Instance for {transientDisposable.Name} is available");
+  }
+  
+}
